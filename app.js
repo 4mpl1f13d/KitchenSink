b7ed65acc12ed852d3eb274f8582f91354527247
unknown <jlpeters@gmail.com>
2020-01-14 16:59:33 -0600

    waiting...

--- app.js
+++ app.js
@@ -1,8 +1,44 @@
-var firstName = 'Jeremy';
+var firstName ='Jeremy';
 const numOfStates = 50;
 
-var a = add(5,4);
+var a = 5 - 4;
     console.log(a);
+    
+function sayHello() {
+    alert('Hello World!')
+}
+
+sayHello();
+
+function checkAge(name, age){
+    if (age < 21) {
+         alert("Sorry "+ name +" you aren't old enough to view this page!")
+    } 
+}
+checkAge("Charles",21);
+checkAge("Abby",27);
+checkAge("James",18);
+checkAge("John",17);
+
+var veggies = ["Bell Peppers", "Cabbage", "Garlic", "Carrots"];
+    console.log(veggies);
+
+var tester = [
+    {name:'Tina', age:19};
+    {name:'Bryan', age:30};
+    {name:'JP', age:1};
+    {name:'Janet', age:50};
+    {name:'Gail', age:14};
+]
+
+function checkAge(tester)
+    for (let i=0,  i< tester.length, i++)
+
+
+
+
+}
+
 
 
 
