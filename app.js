a5425788cc27367a037d59eb0b2f0039a4d39ae6
unknown <jlpeters@gmail.com>
2020-01-15 10:18:03 -0600

    finished!

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ function checkAge(name, age){
     if (age < 21) {
          alert("Sorry "+ name +" you aren't old enough to view this page!")
     } 
-}
+};
+
 checkAge("Charles",21);
 checkAge("Abby",27);
 checkAge("James",18);
@@ -23,23 +24,28 @@ checkAge("John",17);
 var veggies = ["Bell Peppers", "Cabbage", "Garlic", "Carrots"];
     console.log(veggies);
 
-var tester = [
-    {name:'Tina', age:19};
-    {name:'Bryan', age:30};
-    {name:'JP', age:1};
-    {name:'Janet', age:50};
-    {name:'Gail', age:14};
+var tester1 = [
+    {name:'Tina', age:19},
+    {name:'Bryan', age:30},
+    {name:'JP', age:1},
+    {name:'Janet', age:50},
+    {name:'Gail', age:14},
 ]
 
-function checkAge(tester)
-    for (let i=0,  i< tester.length, i++)
+for (var i=0; i<5; i++){
+   checkAge(tester1[i].name, tester1[i].age);
+    //  console.log(tester1[i]);
+};
 
+ var anyWord='Hello World';
+function getLength(anyWord){
+    var n = anyWord.length;    
+    console.log(anyWord.length);
+      if (n%2 == 0){
+            console.log('The world is nice and even!');
+        }else{
+    console.log('The world is an odd place!');
+    }
+};
 
-
-
-}
-
-
-
-
-
+getLength(anyWord);
